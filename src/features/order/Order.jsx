b344b8eb9bde5436515f9b6619e3eb9ed0b6123b
Srdjan <srdjan.s96@outlook.com>
2023-11-12 17:03:05 +0100

    Implement order listing and apply styling

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -5,6 +5,7 @@ import {
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import OrderItem from "./OrderItem";
 
 function Order() {
   const order = useLoaderData();
@@ -48,6 +49,12 @@ function Order() {
           (Estimated delivery : {formatDate(estimatedDelivery)}
         </p>
       </div>
+
+      <ul className="divide-y divide-stone-200 border-b border-t">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.itemId} />
+        ))}
+      </ul>
 
       <div className="space-y-2 bg-stone-200 px-6 py-5">
         <p className="text-sm font-medium text-stone-600">
