a2b9c3bcfd84c9c02cc1545f2c42a950f00a72f6
Srdjan <srdjan.s96@outlook.com>
2023-11-12 14:35:16 +0100

    Adjust button text size

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block rounded-full bg-amber-800 font-semibold uppercase tracking-wide text-stone-100 transition-colors duration-300 hover:bg-amber-600 focus:bg-amber-600 focus:outline-none focus:ring focus:ring-amber-600 focus:ring-offset-2 disabled:cursor-not-allowed ";
+    "inline-block text-sm rounded-full bg-amber-800 font-semibold uppercase tracking-wide text-stone-100 transition-colors duration-300 hover:bg-amber-600 focus:bg-amber-600 focus:outline-none focus:ring focus:ring-amber-600 focus:ring-offset-2 disabled:cursor-not-allowed ";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4 ",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
     secondary:
-      "inline-block rounded-full font-semibold border-2 border-stone-300 uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-amber-600 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
+      "inline-block text-sm rounded-full font-semibold border-2 border-stone-300 uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-amber-600 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
 
   if (to) {
