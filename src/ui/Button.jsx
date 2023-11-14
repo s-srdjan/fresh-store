79363ee35f0c5cc1d2778f6cc86618092a6b3786
Srdjan <srdjan.s96@outlook.com>
2023-11-14 14:18:12 +0100

    Add new button type and implement onClick handler

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     "inline-block text-sm rounded-full bg-yellow-600 font-semibold uppercase tracking-wide text-stone-100 transition-colors duration-300 hover:bg-amber-600 focus:bg-amber-600 focus:outline-none focus:ring focus:ring-amber-600 focus:ring-offset-2 disabled:cursor-not-allowed ";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4 ",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
     secondary:
       "inline-block text-sm rounded-full font-semibold border-2 border-stone-300 uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-amber-600 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
@@ -18,6 +19,13 @@ function Button({ children, disabled, to, type }) {
       </Link>
     );
   }
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
 
   return (
     <button disabled={disabled} className={styles[type]}>
