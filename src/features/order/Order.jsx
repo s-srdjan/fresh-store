8c41d42d77102b4c06e701d5ffb403fa9239694a
Srdjan <srdjan.s96@outlook.com>
2023-11-16 18:34:23 +0100

    Add functionality for updating order priority

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -7,6 +7,7 @@ import {
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
 import { useEffect } from "react";
+import UpdateOrder from "./updateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -87,6 +88,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
